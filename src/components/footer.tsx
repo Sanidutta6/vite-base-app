c9eae45e962d853aa13fce6c59788f87fed7aee9
Suny Dutta <sanidutta6@gmail.com>
2026-02-08 07:35:31 +0530

    Updated header, footer background, text colors, added mode toggle in header.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="border-t border-border px-4 py-3">
+        <footer className="bg-card text-card-foreground border-t border-border p-4">
             <p className="text-center text-sm">&copy; {new Date().getFullYear()}. All rights reserved.</p>
         </footer>
     );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,19 @@
+import { ModeToggle } from "./mode-toggle";
 import { Separator } from "./ui/separator";
 import { SidebarTrigger } from "./ui/sidebar";
 
 export function Header() {
     return (
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="flex items-center justify-center gap-2 px-4">
+        <header className="bg-card text-card-foreground  px-4 flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+            <div className="flex items-center justify-center gap-2">
                 <SidebarTrigger className="-ml-1" />
                 <Separator
                     orientation="vertical"
                     className="mr-2 my-auto border data-[orientation=vertical]:h-4"
                 />
+            </div>
+            <div>
+                <ModeToggle />
             </div>
         </header>
     );
